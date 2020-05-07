5c3c1bc35fc162aefa66f5a9d26f4ffc298165f1
Andrew Telleria <andrew@aktelleria.com>
2020-05-07 13:37:28 -0700

    Order, Item, OrderItem models defined

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+
+# List of items that can be added to an order
+# As soon as an item is added to the cart it becomes OrderItem
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+
+# Links the item to the order
+# Handles specific logic about the order item
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
+# Link order items to the order
+# Essentially a shopping cart
+# Every time they login we fetch the order and display order as shopping cart item count
+# Define boolean field on order if it has been order
+# If not, this order will be used until it is ordered
+# As soon as it is ordered, the next order is created
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -14,6 +14,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'core'
 ]
 
@@ -75,3 +79,11 @@ if ENVIRONMENT == 'production':
     SECURE_REDIRECT_EXEMPT = []
     SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
