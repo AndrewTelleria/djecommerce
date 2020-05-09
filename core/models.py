bf82cab3ad148a872b59b37a175ddee288876919
Andrew K Telleria <andrew@aktelleria.com>
2020-05-08 19:26:59 -0700

    Get absolute URL

--- core/models.py
+++ core/models.py
@@ -1,6 +1,18 @@
 from django.conf import settings
 from django.db import models
+from django.shortcuts import reverse
 
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport Wear'),
+    ('OW', 'Outwear')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
 
 
 # List of items that can be added to an order
@@ -8,9 +20,17 @@ from django.db import models
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={
+            'slug': self.slug
+        })
 
 
 # Links the item to the order
@@ -19,12 +39,13 @@ class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
 
 
-# Link order items to the order
-# Essentially a shopping cart
-# Every time they login we fetch the order and display order as shopping cart item count
-# Define boolean field on order if it has been order
-# If not, this order will be used until it is ordered
-# As soon as it is ordered, the next order is created
+# Link order items to the order.
+# Essentially a shopping cart.
+# Every time they login we fetch the order
+# and display order as shopping cart item count.
+# Define boolean field on order if it has been order.
+# If not, this order will be used until it is ordered.
+# As soon as it is ordered, the next order is created.
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
--- core/views.py
+++ core/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
 
-def item_list(request):
+
+def products(request):
     context = {
         'items': Item.objects.all()
     }
-    return render(request, "item_list.html", context)
+    return render(request, "products.html", context)
+
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
